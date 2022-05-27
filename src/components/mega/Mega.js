204cd7a0bc06dfc4d8507991e814624c57193de5
unknown <lucasouzape@gmail.com>
2022-05-27 15:34:16 -0300

    add Mega Sena

---
+++ src/components/mega/Mega.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {Text} from 'react-native';
+import Estilo from '../estilo'
+
+export default class Mega extends Component {
+
+    render ()  {
+        return(
+            <Text style= {Estilo.txtG}>
+                Gerador de Mega-Sena
+            </Text>
+        )
+    }
+
+}
